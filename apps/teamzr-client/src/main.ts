7325e3b5dbcd9dee663cfb8de228fc0a1ce5327a
Jakub <brockljakub@gmail.com>
2024-02-15 15:03:27 +0100

    Change: Teamzr Client change repository

--- apps/teamzr-client/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { TeamzrClientStoryboardModule } from './teamzr-client-storyboard.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(TeamzrClientStoryboardModule);
-  await app.listen(3000);
-}
-bootstrap();
--- apps/teamzr-client/src/teamzr-client-storyboard.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class TeamzrClientStoryboardService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
